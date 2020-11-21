ec0d7acd45daf71e7760ce88e359a10b0dbae1e5
Paula Pompeo <paulapompeoc@gmail.com>
2020-11-20 22:20:51 -0300

    typeorm + sqlite config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
@@ -9,3 +11,10 @@ app.get('/users', (request, response) => {
 }); 
 
 app.listen(3333);
+
+//Driver nativo, query builder, ORM (niveis de abstracao)
+
+//driver nativo -> sqlite3 (query em sql)
+//query builder -> knex.js (query em js) - da pra trocar sem mexer na applicacao
+//ORM -> typeorm (cada tabela vira uma classe) - da pra trocar sem mexer na applicacao
+// table: user - class: User  -> relacionamento de objeto para cada registro da tabela
